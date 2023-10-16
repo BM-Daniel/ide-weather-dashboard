94c37fc645ce71b83691a43c0c0fb807fff24994
godcandidate <godcandidate@gmail.com>
2023-10-16 20:58:15 +0100

    update weatherController.js changed location to be accepted as a parameter instead of as a body

--- backend/controller/weatherController.js
+++ backend/controller/weatherController.js
@@ -1,7 +1,7 @@
 // controller functions
 
 export async function getCurrentWeather(req, res) {
-    const {location} = req.body;
+    const {location} = req.params;
 
     return res.status(201).send({msg: `Today's weather in ${location} is supper cool`});
   }
